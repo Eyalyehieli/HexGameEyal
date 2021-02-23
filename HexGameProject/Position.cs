4dc22e162941fc099b70687129ec7558b0002921
EyalYehieli <eyal.yehieli2@gmail.com>
2021-02-23 16:31:07 +0200

    fixes

---
+++ HexGameProject/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexGameProject
+{
+    class Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int i,int j)
+        {
+            this.x = i;
+            this.y = j;
+        }
+    }
+}
